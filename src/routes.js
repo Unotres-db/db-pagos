4ef10e8877f84aca5d16c790e657eef8f49a7726
unknown <martincsl@hotmail.com>
2024-10-21 09:52:32 -0300

    eliminacion de controller de transaccionesMes y correcciones en routes

--- src/routes.js
+++ src/routes.js
@@ -1,30 +1,30 @@
 const express = require ('express');
 const routes = express.Router();
 
+const Invoice = require('./controllers/Invoice')
 const Logout = require('./controllers/Logout');
 const Proveedores = require('./controllers/Proveedores');
 const Proyectos = require('./controllers/Proyectos');
+const Reset = require('./controllers/Reset');
 const Rubros = require('./controllers/Rubros');
 const Session = require('./controllers/Session');
 const TestDates = require('./controllers/TestDates');
 const Transacciones = require('./controllers/Transacciones');
-const Reset = require('./controllers/Reset')
 const Users = require('./controllers/Users');
-const Invoice = require('./controllers/Invoice')
 
 routes.get ('/factura', Invoice.index);
 
 routes.post ('/logout', Logout.create);
-
-routes.get ('/proyectos/:id?', Proyectos.index);
-routes.post ('/proyectos', Proyectos.create);
-routes.post ('/proyectos-bulk', Proyectos.createBulk);
 
 routes.get ('/proveedores/:id?', Proveedores.index);
 routes.post ('/proveedores', Proveedores.create);
 routes.post ('/proveedores-bulk', Proveedores.createBulk);
 routes.delete ('/proveedores/:id?', Proveedores.delete);
 routes.delete ('/proveedores-all', Proveedores.deleteAllRecordsFromProveedores);
+
+routes.get ('/proyectos/:id?', Proyectos.index);
+routes.post ('/proyectos', Proyectos.create);
+routes.post ('/proyectos-bulk', Proyectos.createBulk);
 
 routes.post ('/reset-email', Reset.create);
 // routes.post ('/code-by-email', CodeByEmailController.create);
@@ -43,7 +43,6 @@ routes.put ('/transacciones', Transacciones.update);
 routes.delete ('/transacciones/:id?', Transacciones.delete);
 routes.delete ('/transacciones-all', Transacciones.deleteAllRecordsFromTransacciones);
 
-// deleteAllRecordsFromTransacciones
 routes.get ('/test-dates/:id?', TestDates.index);
 routes.post ('/test-dates', TestDates.create);
 routes.delete ('/test-dates/:id?', TestDates.delete);
